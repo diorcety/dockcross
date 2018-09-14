5289337e15b9ad5ef3239f58eb83e5121fa2e429
Yann Diorcet <yann.diorcet@xenocs.com>
2018-09-14 12:41:48 +0200

    Add xmanylinux-x86

---
+++ xmanylinux-x86/Toolchain.cmake
@@ -0,0 +1,14 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_VERSION 1)
+set(CMAKE_SYSTEM_PROCESSOR i686)
+set(CMAKE_CROSSCOMPILING FALSE)
+
+set(cross_triple "i686-linux-gnu")
+
+set(CMAKE_LINKER /opt/llvm60/bin/ld.lld)
+set(CMAKE_C_COMPILER /opt/llvm60/bin/clang)
+set(CMAKE_CXX_COMPILER /opt/llvm60/bin/clang++)
+set(CMAKE_ASM_COMPILER ${CMAKE_C_COMPILER})
+set(CMAKE_Fortran_COMPILER /opt/rh/devtoolset-2/root/usr/bin/gfortran)
+
+set(CMAKE_CROSSCOMPILING_EMULATOR /usr/bin/${cross_triple}-noop)
---
+++ xmanylinux-x86/fix.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd /opt/rh/devtoolset-2/root/usr/lib/gcc
+ln -s i386-CentOS-linux/ i386-redhat-linux
+cd /opt/rh/devtoolset-2/root/usr/include/c++/4.8.2/
+ln -s i386-CentOS-linux/ i386-redhat-linux
