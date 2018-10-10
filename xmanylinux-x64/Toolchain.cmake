60d5daacf1eb411718db2abbf47a9b7d55211dae
Yann Diorcet <yann.diorcet@xenocs.com>
2018-10-10 18:29:49 +0200

    Remove libffi

--- xmanylinux-x64/Toolchain.cmake
+++ xmanylinux-x64/Toolchain.cmake
@@ -9,6 +9,7 @@ set(CMAKE_LINKER /opt/llvm60/bin/ld.lld)
 set(CMAKE_C_COMPILER /opt/llvm60/bin/clang)
 set(CMAKE_CXX_COMPILER /opt/llvm60/bin/clang++)
 set(CMAKE_ASM_COMPILER ${CMAKE_C_COMPILER})
+set(CMAKE_ASM_COMPILER_ID Clang)
 set(CMAKE_Fortran_COMPILER /opt/rh/devtoolset-2/root/usr/bin/gfortran)
 
 set(CMAKE_CROSSCOMPILING_EMULATOR /usr/bin/${cross_triple}-noop)
