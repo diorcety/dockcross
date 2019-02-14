cbbf09070679f83f0fa23bab116c32195dd42857
Yann Diorcet <yann.diorcet@xenocs.com>
2019-02-14 13:23:29 +0100

    Fix images for i686

--- xmanylinux-common/fix.sh
+++ xmanylinux-common/fix.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
-cd /opt/rh/devtoolset-2/root/usr/lib/gcc
-ln -s x86_64-CentOS-linux/ x86_64-redhat-linux
-cd /opt/rh/devtoolset-2/root/usr/include/c++/4.8.2/
-ln -s x86_64-CentOS-linux/ x86_64-redhat-linux
+
+MACHINE_TYPE=`uname -m`
+if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+    cd /opt/rh/devtoolset-2/root/usr/lib/gcc
+    ln -s x86_64-CentOS-linux/ x86_64-redhat-linux
+    cd /opt/rh/devtoolset-2/root/usr/include/c++/4.8.2/
+    ln -s x86_64-CentOS-linux/ x86_64-redhat-linux
+else
+    cd /opt/rh/devtoolset-2/root/usr/lib/gcc
+    ln -s i386-CentOS-linux/ i386-redhat-linux
+    cd /opt/rh/devtoolset-2/root/usr/include/c++/4.8.2/
+    ln -s i386-CentOS-linux/ i386-redhat-linux
+fi
