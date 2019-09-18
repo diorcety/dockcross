8f225d530ea69fffab0b193de613673e4b098cf4
Yann Diorcet <yann.diorcet@xenocs.com>
2019-09-18 14:42:34 +0200

    fix.sh: fix the wrong arch detection

--- xmanylinux-common/fix.sh
+++ xmanylinux-common/fix.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-MACHINE_TYPE=`uname -m`
-if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+if test ${LIB_DIR} = "lib64"; then
+    echo "Arch x86_64"
     cd /opt/rh/devtoolset-2/root/usr/lib/gcc
     ln -s x86_64-CentOS-linux/ x86_64-redhat-linux
     cd /opt/rh/devtoolset-2/root/usr/include/c++/4.8.2/
     ln -s x86_64-CentOS-linux/ x86_64-redhat-linux
 else
+    echo "Arch i686"
     cd /opt/rh/devtoolset-2/root/usr/lib/gcc
     ln -s i386-CentOS-linux/ i386-redhat-linux
     cd /opt/rh/devtoolset-2/root/usr/include/c++/4.8.2/
